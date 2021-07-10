dd7da0080b50abb36a329ae20ce3d5ae5c219598
Michael DiPietro <mdipietro@wayfair.com>
2021-07-10 17:20:16 -0400

    Module 4

--- ssg.py
+++ ssg.py
@@ -7,7 +7,7 @@ def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": [ssg.parsers.ResourceParser()]
+        "parsers": [ssg.parsers.ResourceParser(), ssg.parsers.ReStructuredTextParser(), ssg.parsers.MarkdownParser()]
     }
 
     Site(**config).build()
